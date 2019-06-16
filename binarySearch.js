922eb536cc7f17ba90ffc9b1f72eea7134907066
Samer <samaladhami@gmail.com>
2019-06-15 21:49:12 -0500

    cleanup

--- binarySearch.js
+++ binarySearch.js
@@ -5,11 +5,11 @@ function binarySearch(arr, n) {
   if(arr[0] === n) return true;
   if(arr.length <= 1 && n !== arr[0]) return false;
   
-  let middleIndex = Math.floor(arr.length / 2)
-  if(n < arr[middleIndex]) {
-    return binarySearch(arr.slice(0, middleIndex), n)
+  let median = Math.floor(arr.length / 2)
+  if(n < arr[median]) {
+    return binarySearch(arr.slice(0, median), n)
   } else {
-    return binarySearch(arr.slice(middleIndex), n)
+    return binarySearch(arr.slice(median), n)
   }
 }
 
